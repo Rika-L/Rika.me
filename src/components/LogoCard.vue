8e4244a26539dfaa977b147a763a9ed024e531b1
Rika-L <2209393334@qq.com>
2024-08-08 16:09:29 +0800

    feat: 修改logo组件行距

--- src/components/LogoCard.vue
+++ src/components/LogoCard.vue
@@ -16,7 +16,7 @@ defineProps({
 
 <template>
   <a :href="url"
-     class="inline-block mx-2 text-gray-500 dark:bg-[#fff2] bg-[#0000000f] hover:bg-[#0000001f] dark:hover:bg-[#fff3] py-1 px-2 rounded cursor-pointer"><span
+     class="inline-block my-0.5 mx-2 text-gray-500 dark:bg-[#fff2] bg-[#0000000f] hover:bg-[#0000001f] dark:hover:bg-[#fff3] py-1 px-2 rounded cursor-pointer"><span
       class="iconify-color text-xl align-middle"
       :class="logo"
   ></span><span>&nbsp;{{ title }}</span></a>
