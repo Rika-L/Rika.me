292ae120c82592f150e9578eb259369fd2b66537
Rika-L <2209393334@qq.com>
2024-09-24 16:32:05 +0800

    修复在star背景z-index的问题

--- src/components/ArtStar.vue
+++ src/components/ArtStar.vue
@@ -1,21 +1,19 @@
 <script setup lang="ts">
-
-import {onMounted, onUnmounted} from "vue";
-import BackgroundStar from "@/components/BackgroundStar.vue";
-import random from "@/utils/random.ts";
+import { onMounted, onUnmounted } from 'vue'
+import BackgroundStar from '@/components/BackgroundStar.vue'
+import random from '@/utils/random.ts'
 
 let pX = $ref<null | number>(null)
 let pY = $ref<null | number>(null)
 
-const positions = Array.from({length: 50}, () => [random(0,100), random(0,100)]);
+const positions = Array.from({ length: 50 }, () => [random(0, 100), random(0, 100)])
 
-function handleMove({pageX, pageY}: { pageX: number, pageY: number }) {
+function handleMove({ pageX, pageY }: { pageX: number, pageY: number }) {
   pX = pageX
   pY = pageY
 }
 
 onMounted(() => {
-  console.log(positions);
   document.body.addEventListener('mousemove', handleMove)
 })
 
@@ -25,8 +23,10 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <section class="absolute w-screen h-screen">
-    <background-star v-for="(item,index) in positions" :key="index" :pageX="pX" :pageY="pY" :size="index/20 + 4"
-                     :top="`${item[0]}%`" :left="`${item[1]}%`"/>
+  <section class="absolute w-screen h-screen -z-10">
+    <BackgroundStar
+      v-for="(item, index) in positions" :key="index" :page-x="pX" :page-y="pY" :size="index / 20 + 4"
+      :top="`${item[0]}%`" :left="`${item[1]}%`"
+    />
   </section>
 </template>
