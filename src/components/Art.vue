b3c8be329d5e7d213a72b62b839c4dbb60bf8093
Rika-L <2209393334@qq.com>
2024-09-28 19:45:18 +0800

    arAurora

--- src/components/Art.vue
+++ src/components/Art.vue
@@ -6,12 +6,16 @@
  */
 import { computed, defineAsyncComponent } from 'vue'
 
-const artArr = ['plum', 'ball', 'star']
+const artArr = ['plum', 'ball', 'star', 'aurora']
 
 function getArt(arr: string[]): string {
-  return Math.random() > 0.3
+  return Math.random() > 0.75
     ? arr[0]
-    : Math.random() > 0.5 ? arr[1] : arr[2]
+    : Math.random() > 0.66
+      ? arr[1]
+      : Math.random() > 0.5
+        ? arr[2]
+        : arr[3]
 }
 
 const ArtComponent = computed(() => {
@@ -19,6 +23,8 @@ const ArtComponent = computed(() => {
   if (typeof window !== 'undefined') {
     if (art === 'plum')
       return defineAsyncComponent(() => import('./ArtPlum.vue'))
+    else if (art === 'aurora')
+      return defineAsyncComponent(() => import('./ArtAurora.vue'))
     else if (art === 'ball')
       return defineAsyncComponent(() => import('./ArtBall.vue'))
     else if (art === 'star')
---
+++ src/components/ArtAurora.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="fixed top-0 bottom-0 left-0 right-0 -z-[1]">
+    <div class="bg" />
+  </div>
+</template>
+
+<style scoped>
+.bg {
+  --transparent: transparent;
+  --gradient: repeating-linear-gradient(100deg, var(--c-bg) 0%, var(--c-bg) 7%, var(--transparent) 10%, var(--transparent) 12%, var(--c-bg) 16%);
+  --aurora: repeating-linear-gradient(100deg, #3b82f6 10%, #86efac 15%, #93c5fd 20%, #a5f3fc 25%, #60a5fa 30%);
+  background-image: var(--gradient), var(--aurora);
+  background-size: 300%, 200%;
+  background-position: 50% 50%, 50% 50%;
+  filter: blur(10px) invert(1);
+  position: absolute;
+  inset: -10px;
+  opacity: 0.5;
+  will-change: transform;
+  mask-image: radial-gradient(ellipse at 100% 0%, black 10%, var(--transparent) 70%);
+}
+
+.dark .bg {
+  filter: invert(0);
+}
+
+.bg::after {
+  content: '';
+  position: absolute;
+  inset: 0;
+  background-image: var(--gradient), var(--aurora);
+  background-size: 200%, 100%;
+  background-attachment: fixed;
+  mix-blend-mode: difference;
+  pointer-events: none;
+  animation: aurora 60s linear infinite;
+}
+
+@keyframes aurora {
+  from {
+    background-position: 50% 50%, 50% 50%;
+  }
+
+  to {
+    background-position: 350% 50%, 350% 50%;
+  }
+}
+</style>
